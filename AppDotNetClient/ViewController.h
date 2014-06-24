8b4a905c3266cba54b8bc78b1e477f8a9e8db951
Ansoneh <anson9@gmail.com>
2014-06-24 11:07:17 -0500

    Initial Commit

---
+++ AppDotNetClient/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AppDotNetClient
+//
+//  Created by Anson Brown on 6/24/14.
+//  Copyright (c) 2014 Anson Brown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AppDotNetClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppDotNetClient
+//
+//  Created by Anson Brown on 6/24/14.
+//  Copyright (c) 2014 Anson Brown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
